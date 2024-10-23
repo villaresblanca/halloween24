5cc9b88b8e367353881396c88f6289c7a71e58ee
villaresblanca <villaresblanca09@gmail.com>
2024-10-23 14:15:50 +0100

    animacion funciona

--- js/script.js
+++ js/script.js
@@ -7,16 +7,18 @@
 
     // Al hacer click en la imagen, se muestra el modal
     img.onclick = function () {
-        modal.style.display = "block";
-        modal.classList.add("show"); //Añadir clase para iniciar la animación
-        modalImg.src = this.src; // Carga la imagen clicada en el modal
+        modal.style.display = "block"; //Muestra el modal
+        modalImg.src = this.src; //Cargar la imagen cliclada en el modal
+        setTimeout(function() {
+            modal.classList.add("show"); //Añadir clase despues de un pequeño retraso para iniciar la animación
+        }, 10); //Pequeño retraso de 10ms para permitir que se renderice el modal antes de la animación
     }
 
     // Al hacer click en la "X" se cierra el modal
     span.onclick = function () {
         modal.classList.remove("show"); //Quita la clase de la animación
         setTimeout(function() {
-            modal.style.display = "none";
+            modal.style.display = "none"; //Espera que termine la animación antes de ocultar el modal
         }, 400); //Espera a que termine la animación antes de ocultar el modal
     }
 
