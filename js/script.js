ac05fac8dd3a9a97a23e4f8becf8ec6710032548
villaresblanca <villaresblanca09@gmail.com>
2024-10-24 09:08:55 +0100

    actualizar animacion

--- js/script.js
+++ js/script.js
@@ -5,21 +5,31 @@
     var modalImg = document.getElementById("img-modal-jubilado");
     var span = document.getElementsByClassName("close")[0];
 
-    // Al hacer click en la imagen, se muestra el modal
-    img.onclick = function () {
-        modal.style.display = "block"; //Muestra el modal
+    //Al hacer click en la imagen, se muestra el modal
+    img.onclick = function () { 
+        var rect = img.getBoundingClientRect(); //Obtener posición de la imagen
         modalImg.src = this.src; //Cargar la imagen cliclada en el modal
+
+        //Muestra el modal
+        modal.style.display = "block";
+
+        //Ajusta elpunto de origen del zoom según la posición de la imagen
+        modalImg.style.transformOrigin = `${rect.left + window.scrollX}px ${rect.top + window.scrollY}px`;
+
+        //Añadir clase despues de un pequeño retraso para iniciar la animación
         setTimeout(function() {
-            modal.classList.add("show"); //Añadir clase despues de un pequeño retraso para iniciar la animación
+            modal.classList.add("show");
         }, 10); //Pequeño retraso de 10ms para permitir que se renderice el modal antes de la animación
     }
 
     // Al hacer click en la "X" se cierra el modal
     span.onclick = function () {
         modal.classList.remove("show"); //Quita la clase de la animación
+
+        //Espera que termine la animación antes de ocultar el modal
         setTimeout(function() {
-            modal.style.display = "none"; //Espera que termine la animación antes de ocultar el modal
-        }, 400); //Espera a que termine la animación antes de ocultar el modal
+            modal.style.display = "none"; 
+        }, 400);
     }
 
     // Para que se cierre la imagen haciendo clik fuera de esta y no en la "X"
@@ -28,6 +38,6 @@
             modal.classList.remove("show"); //Quita la clase de animación
             setTimeout(function() {
                 modal.style.display = "none";
-            }, 400); //Espera a que terminela animación antes de ocultar el modal
+            }, 400); //Espera a que termine la animación antes de ocultar el modal
         }
     }
